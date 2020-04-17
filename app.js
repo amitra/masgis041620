f2f195d97ae2337702709cec3cbb536593080182
Ayan Mitra <mitra.ayan@gmail.com>
2020-04-16 19:13:52 -0700

    Initial

---
+++ app.js
@@ -0,0 +1,18 @@
+/* START WRITING YOUR JS HERE */
+// single line commment
+/*
+line1
+line2
+*/
+
+var myTitle = document.title;
+console.log(myTitle);
+
+myTitle = 'Blah Blah Blah';
+
+//this sets tthe documents title property
+document.title = myTitle;
+
+console.log(document.title);
+
+document.getElementById('ayanDiv').innerHTML = 'some regular text';
