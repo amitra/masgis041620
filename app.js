d3a0867f87a034c21cbe6b4a47a7b908144503d0
Ayan Mitra <mitra.ayan@gmail.com>
2020-04-16 21:03:43 -0700

    Add additional info

--- app.js
+++ app.js
@@ -3,11 +3,14 @@
 
 var myMap;
 
-//import the esri map library using dojo and then run a callback function
+//import the esri map library and then run a callback function
 require( ["esri/map"] , function(Map){
 //code starts here
 //Map is a bluprint or a recipie for creating more map objects
 
+//when you create a new map, you need two things
+//an id of an  element that acts as a container, in this case a div with an id of myMapContainer
+//AND an options object that has all the various options like type of basemap, center, zoom etc
 myMap = new Map("myMapContainer" , {
     basemap:'topo',
     center: [-122.25, 37.75],
