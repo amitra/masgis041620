d86de30d65e6ced8f3c9128d42366dfec2afb2f7
Ayan Mitra <mitra.ayan@gmail.com>
2020-04-16 19:33:36 -0700

    Initial

--- app.js
+++ app.js
@@ -16,3 +16,17 @@ document.title = myTitle;
 console.log(document.title);
 
 document.getElementById('ayanDiv').innerHTML = 'some regular text';
+
+//calling my function
+var myFnOutput = concatenateText('Ayan', 'Mitra');
+//print this variable out to the console
+console.log(myFnOutput);
+
+//Function declaration
+function concatenateText(word1, word2){
+    //concatenates two numbers
+    var fullWord = word1 + ' ' + word2;
+    
+    //returns some output
+    return fullWord;
+}
